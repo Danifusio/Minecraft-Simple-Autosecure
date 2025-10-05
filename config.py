4347cc0f3ee2da818521d5c94e4548d8cc2baf59
Danifusio <daniregueirosoler@gmail.com>
2025-10-05 17:42:10 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "TOKEN_HERE"
+API_KEY = "HYPIXEL_API_KEY_HERE"
+MAILSLURP_API_KEY="MAILSLURP_API_HERE"
+
+
+#Dont Fill These In
+LastUsedEmail=""
+LastUserName=""
+LastRecoveryCode=""
+AUTHVALUE=""
